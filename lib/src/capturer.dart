7fa1e14ea8ae0e320a5801f2af4c343d641f3c02
Parth Virani - Ocean M Tech <parthvirani.oceanmtech@gmail.com>
2024-10-07 10:42:15 +0530

    Update capturer.dart

--- lib/src/capturer.dart
+++ lib/src/capturer.dart
@@ -271,8 +271,8 @@ class RenderCapturer<K extends RenderFormat> {
         view: flutterView,
         child: RenderPositionedBox(alignment: Alignment.center, child: repaintBoundary),
         configuration: ViewConfiguration(
+          physicalConstraints: BoxConstraints.tight(logicalSize) * flutterView.devicePixelRatio,
           logicalConstraints: BoxConstraints.tight(logicalSize),
-          physicalConstraints: BoxConstraints.tight(logicalSize * (session.settings.pixelRatio)),
           devicePixelRatio: session.settings.pixelRatio,
         ),
       );
