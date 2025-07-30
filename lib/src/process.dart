7fe81e35e3b19a2dc98e8147d1fea84833cfea5c
JaydipNasitOceanmtech <jaydipnasit.oceanmtech@gmail.com>
2025-07-30 15:19:18 +0530

    ffmpeg_kit_flutter_new: ^3.1.0
      ffmpeg_kit_flutter_new: ^3.1.0

--- lib/src/process.dart
+++ lib/src/process.dart
@@ -1,9 +1,9 @@
 import 'dart:io';
 
-import 'package:ffmpeg_kit_16kb/ffmpeg_kit_config.dart';
-import 'package:ffmpeg_kit_16kb/ffmpeg_session.dart';
-import 'package:ffmpeg_kit_16kb/log.dart';
-import 'package:ffmpeg_kit_16kb/statistics.dart';
+import 'package:ffmpeg_kit_flutter_new/ffmpeg_kit_config.dart';
+import 'package:ffmpeg_kit_flutter_new/ffmpeg_session.dart';
+import 'package:ffmpeg_kit_flutter_new/log.dart';
+import 'package:ffmpeg_kit_flutter_new/statistics.dart';
 import 'package:render/src/formats/abstract.dart';
 import 'package:render/src/service/notifier.dart';
 import 'package:render/src/service/session.dart';
